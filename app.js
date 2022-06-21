fa2c02e08942ba023c84a5cf98fd489b31dcca86
zachary webb <zachsavn@gmail.com>
2022-06-21 14:49:52 -0400

    completed exercise 1

--- app.js
+++ app.js
@@ -2,6 +2,18 @@ console.log("Hello World!\n==========\n");
 
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
+let isOdd = 0;
+
+for(isOdd == 0; isOdd < 100; isOdd++) {
+   // Checking if the number is odd.
+    if (isOdd % 2 == 1){
+        console.log(isOdd);
+    } else {
+        continue;
+    }
+};
 
 // Exercise 2 Section
 console.log("EXERCISE 2:\n==========\n");
+
+let fizzBuzz = 0;
