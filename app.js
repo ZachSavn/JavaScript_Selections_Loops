016568d2a2595936a903265972f70c98ff14a792
zachary webb <zachsavn@gmail.com>
2022-06-21 15:21:50 -0400

    deleted some comments

--- app.js
+++ app.js
@@ -4,14 +4,14 @@ console.log("Hello World!\n==========\n");
 console.log("EXERCISE 1:\n==========\n");
 let isOdd = 0;
 
-for(isOdd == 0; isOdd < 100; isOdd++) {
-   // Checking if the number is odd.
-    if (isOdd % 2 == 1){
-        console.log(isOdd);
-    } else {
-        continue;
-    }
-};
+for (isOdd == 0; isOdd < 100; isOdd++) {
+  // Checking if the number is odd.
+  if (isOdd % 2 == 1) {
+    console.log(isOdd);
+  } else {
+    continue;
+  }
+}
 
 // Exercise 2 Section
 console.log("EXERCISE 2:\n==========\n");
@@ -19,14 +19,16 @@ console.log("EXERCISE 2:\n==========\n");
 let fizzBuzz = 0;
 
 for (fizzBuzz = 0; fizzBuzz < 100; fizzBuzz++) {
-//checking divisibility of number
-    if (fizzBuzz % 3 == 0 && fizzBuzz % 5 == 0) {
+  //checking divisibility of number
+  if (fizzBuzz % 3 == 0 && fizzBuzz % 5 == 0) {
     console.log("FIZZBUZZ");
-    } else if (fizzBuzz % 3 == 0) {
-        console.log("FIZZ");
-    } else if (fizzBuzz % 5 == 0) {
-        console.log("BUZZ");
-    } else {
-        console.log(fizzBuzz);
-    }
-}
+  } else if (fizzBuzz % 3 == 0) {
+    console.log("FIZZ");
+  } else if (fizzBuzz % 5 == 0) {
+    console.log("BUZZ");
+  } else {
+    console.log(fizzBuzz)}/*  else if (fizzBuzz % 3 == 0 && fizzBuzz % 5 == 0) {
+    console.log("FIZZBUZZ");
+  } else {
+    console.log(fizzBuzz); */
+  }
